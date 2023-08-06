929c76fb5a6d93907197e5eea46cbf750db8f914
RavneetDTU <ravneet.dtu@gmail.com>
2023-08-06 11:03:14 +0000

    Adding czech-cs, danish-da, hebrew-ha, polish-pl, swedish-sv, thai-th

---
+++ alembic/versions/4c346a2ce1fd_adding_czech_cs_danish_da_hebrew_ha_.py
@@ -0,0 +1,24 @@
+"""Adding czech-cs, danish-da, hebrew-ha, polish-pl, swedish-sv, thai-th
+
+Revision ID: 4c346a2ce1fd
+Revises: c26b3ea6d99c
+Create Date: 2023-08-06 11:02:11.429361
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4c346a2ce1fd'
+down_revision = 'c26b3ea6d99c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
